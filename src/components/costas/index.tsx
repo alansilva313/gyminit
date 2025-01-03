342f3883c2b7f50bffadf0626ba3f420908a753b
alansilva313 <allansillva313@gmail.com>
2025-01-03 10:24:28 -0300

    remada curvada

--- src/components/costas/index.tsx
+++ src/components/costas/index.tsx
@@ -2,6 +2,7 @@
 
 import costas1 from '@/assets/costas1.gif'
 import costas2 from '@/assets/costas2.gif'
+import costas3 from '@/assets/costas3.gif'
 
 export function Costas(){
 
@@ -20,6 +21,13 @@ export function Costas(){
             description: ""
         },
 
+        {
+            id: 2,
+            title: 'Remada curvada',
+            gif: costas3,
+            description: ""
+        },
+
     ]
 
 
@@ -31,7 +39,7 @@ export function Costas(){
           {treinosCostas.map((item: any) => (
             <div key={item.id} className='flex flex-col gap-4'>
 
-                <img src={item.gif} alt="" className='w-[250px]'/>
+                <img src={item.gif} alt={item.title} className='w-[250px]'/>
                 <p className='font-bold'>{item.title}</p>
                 <p>{item.description}</p>
                 <hr />
