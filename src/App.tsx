14734b3ca298f0b03f54b1ccb7eeb675f70af347
alansilva313 <allansillva313@gmail.com>
2025-01-03 18:23:18 -0300

    treinos

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,25 @@ function App() {
       <h3 className="text-2xl font-bold text-center mb-4">Treinos</h3>
 
       <Tabs defaultValue="costas" className="w-full max-w-[600px] mx-auto mt-10">
-        <TabsList className="flex justify-center gap-4">
+        <TabsList
+          className="flex justify-center gap-4 bg-white shadow-md sticky top-0 z-10"
+        >
           <TabsTrigger value="costas" className="text-sm md:text-base">
             Costas
           </TabsTrigger>
+
+
           <TabsTrigger value="peitoral" className="text-sm md:text-base">
             Peitoral
           </TabsTrigger>
+
+          <TabsTrigger value="pernas" className="text-sm md:text-base">
+            Pernas
+          </TabsTrigger>
         </TabsList>
+
+
+        
 
         <TabsContent value="costas" className="mt-6">
           <Costas />
@@ -22,6 +33,10 @@ function App() {
 
         <TabsContent value="peitoral" className="mt-6 text-center">
           <p>Treino de Peitoral</p>
+        </TabsContent>
+
+        <TabsContent value="pernas" className="mt-6 text-center">
+          <p>Treino de Pernas</p>
         </TabsContent>
       </Tabs>
     </div>
